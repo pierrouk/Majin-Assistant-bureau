c4de755054304229732f3fd8695520eea6a2c974
Pierrick remoleur <pierrick.redmi@gmail.com>
2025-12-02 13:00:31 +0100

    prod actuel

--- include/pins.h
+++ include/pins.h
@@ -2,16 +2,16 @@
 #define PINS_H
 
 // --- 📺 ÉCRAN TFT (Validé par le test Damier) ---
-#define PIN_SCLK        12
-#define PIN_MOSI        11
-#define PIN_MISO        14
-#define PIN_TFT_CS      10
-#define PIN_TFT_DC      9
-#define PIN_TFT_RST     8
-#define PIN_TFT_BL      7
+#define PIN_SCLK        12//SCK
+#define PIN_MOSI        11//SDI(MOSI)
+#define PIN_MISO        14 //SDO(MISO)
+#define PIN_TFT_CS      10 //LCD_CS
+#define PIN_TFT_DC      9//LCD_RS
+#define PIN_TFT_RST     8//LCD_RST
+#define PIN_TFT_BL      7//LED
 
 // --- 💾 CARTE SD (Désactivée pour l'instant) ---
-#define PIN_SD_CS       18
+#define PIN_SD_CS       18//SD_CS
 
 // --- 🌐 BUS I2C ---
 #define ESP32_I2C_SDA_PIN 5
@@ -21,7 +21,7 @@
 #define PIN_AHT_SDA     ESP32_I2C_SDA_PIN
 #define PIN_AHT_SCL     ESP32_I2C_SCL_PIN
 #define PIN_FT_SDA      16
-#define PIN_FT_SCL      15
+#define PIN_FT_SCL      15//CTP_SCL
 
 #define PIN_FT_RST      4
 #define PIN_FT_INT      17
